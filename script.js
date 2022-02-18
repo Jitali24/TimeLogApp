8b8022ba2b8206d6501368be801faef9c2b4573f
Jitali Hadiya <jitalihhadiya@gmail.com>
2022-02-18 18:15:27 +0530

    fourth commit

--- script.js
+++ script.js
@@ -30,25 +30,45 @@ var table = document.getElementById('table2')
 var selectedrow = null
 var flag = false
 
+display()
+
 function addRecord() {
     flag = calculate()
     if (flag === -1) {
         return
     }
-    let newRow = table.insertRow(table.rows.length)
-    let cell1 = newRow.insertCell(0)
-    let cell2 = newRow.insertCell(1)
-    let cell3 = newRow.insertCell(2)
-    let cell4 = newRow.insertCell(3)
-    let cell5 = newRow.insertCell(4)
+    let entries = new Array()
+    entries = JSON.parse(localStorage.getItem('record'))
+        ? JSON.parse(localStorage.getItem('record'))
+        : []
+    entries.push({
+        Id: indexOf(),
+        Start_Time: document.getElementById('start-time').value,
+        End_Time: document.getElementById('end-time').value,
+        Minutes: flag,
+        Task_Description: document.getElementById('task-description').value,
+    })
+    localStorage.setItem('record', JSON.stringify(entries))
+    // let newRow = table.insertRow(table.rows.length)
+    // let cell1 = newRow.insertCell(0)
+    // let cell2 = newRow.insertCell(1)
+    // let cell3 = newRow.insertCell(2)
+    // let cell4 = newRow.insertCell(3)
+    // let cell5 = newRow.insertCell(4)
 
-    cell1.innerHTML = startTime.value
-    cell2.innerHTML = endTime.value
-    cell3.innerHTML = flag
-    cell4.innerHTML = taskDescription.value
-    cell5.innerHTML =
-        '<button  class="btn1" onclick="edit(this);">Edit</button> <button class="btn1" onclick="delete_record(this);">Delete</button>'
-    clear()
+    // cell1.innerHTML = startTime.value
+    // cell2.innerHTML = endTime.value
+    // cell3.innerHTML = flag
+    // cell4.innerHTML = taskDescription.value
+    // cell5.innerHTML =
+    //     '<button  class="btn1" onclick="edit(this);">Edit</button> <button class="btn1" onclick="delete_record(this);">Delete</button>'
+    // clear()
+}
+
+function indexOf() {
+    var table = document.getElementById('table2')
+    var tl = table.rows.length
+    return tl
 }
 
 function clear() {
@@ -109,6 +129,7 @@ function add() {
     }
     clear()
     totalHM()
+    location.reload()
 }
 
 function load() {
@@ -146,4 +167,49 @@ function totalHM() {
     hour = min / 60
     document.getElementById('minutes').innerHTML = min
     document.getElementById('hours').innerHTML = hour
+}
+
+//local storage
+
+// let addbtn = document.getElementById('add')
+
+// addbtn.addEventListener('click', function () {
+//     stval = startTime.value
+//     let record = localStorage.getItem('localdata')
+//     if (localdata == null) {
+//         new_record = []
+//     } else {
+//         new_record = JSON.parse(record)
+//     }
+//     new_record.push(stval)
+//     localStorage.setItem('localdata', JSON.stringify(new_record))
+// })
+
+function display() {
+    var table = document
+        .getElementById('table2')
+        .getElementsByTagName('tbody')[0]
+    let allEntries = localStorage.getItem('record')
+    if (allEntries == null) {
+        entries = []
+    } else {
+        entries = JSON.parse(allEntries)
+    }
+    entries.forEach((item, index) => {
+        var newRow = table.insertRow(table.length)
+
+        cell1 = newRow.insertCell(0)
+        cell1.innerHTML = entries[index].Start_Time
+        cell2 = newRow.insertCell(1)
+        cell2.innerHTML = entries[index].End_Time
+        cell3 = newRow.insertCell(2)
+        cell3.innerHTML = entries[index].Minutes
+        cell4 = newRow.insertCell(3)
+        cell4.innerHTML = entries[index].Task_Description
+        cell5 = newRow.insertCell(4)
+        // cell5.innerHTML = <a onclick='onEdit(this)'>Edit</a> & nbsp
+        // ;<a onClick='onDelete(this)'>Delete</a>
+        cell5.innerHTML =
+            '<button  class="btn1" onclick="edit(this);">Edit</button> <button class="btn1" onclick="delete_record(this);">Delete</button>'
+    })
 }
